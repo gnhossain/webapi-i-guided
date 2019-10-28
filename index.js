2e6b6e3098dd0fa5e478d10a6b257b75241b2e22
G H <gh@Gs-MacBook-Pro.local>
2019-10-27 23:36:31 -0700

    get

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 const express = require('express');
+const hubsModel = require('./data/hubs-model.js');
+const server = express();
+
+server.get('/', (req, res) => {
+    // express will pass the request and response objects to this function
+    // the .send() on the response object can be used to send a response to the client
+    res.send('Hello node 23');
+  });
+
+  server.get('/hubs', (req, res) => {
+    // route handler code here
+    hubsModel
+    .find()
+    .then(hubs => 
+        res.send(hubs);
+  })
+     .catch(error => {
+         res.send(error);
+     });
+      
+  });
+
+const port = 8000;
+
+server.listen(8000, () =>
+  console.log(`\n** API on port ${port} **\n`)
+);
+
