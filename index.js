b912fb121e4e7711559c532b95fb3de86228fbfb
G H <gh@Gs-MacBook-Pro.local>
2019-10-28 14:36:17 -0700

    guided

--- index.js
+++ index.js
@@ -1,29 +1,30 @@
-const express = require('express');
-const hubsModel = require('./data/hubs-model.js');
-const server = express();
+// const express = require('express');
+// const hubsModel = require('./data/hubs-model.js');
+// const server = express();
 
-server.get('/', (req, res) => {
-    // express will pass the request and response objects to this function
-    // the .send() on the response object can be used to send a response to the client
-    res.send('Hello node 23');
-  });
+// server.get('/', (req, res) => {
+//     // express will pass the request and response objects to this function
+//     // the .send() on the response object can be used to send a response to the client
+//     res.send('Hello node 23');
+//   });
 
-  server.get('/hubs', (req, res) => {
-    // route handler code here
-    hubsModel
-    .find()
-    .then(hubs => 
-        res.send(hubs);
-  })
-     .catch(error => {
-         res.send(error);
-     });
+//   server.get('/hubs', (req, res) => {
+//     // route handler code here
+//     hubsModel
+//     .find()
+//     .then(hubs => 
+//         res.send(hubs);
+//   })
+//      .catch(error => {
+//          res.send(error);
+//      });
       
-  });
+//   });
 
-const port = 8000;
+// const port = 8000;
 
-server.listen(8000, () =>
-  console.log(`\n** API on port ${port} **\n`)
-);
+// server.listen(8000, () =>
+//   console.log(`\n** API on port ${port} **\n`)
+// );
 
+console.log("It's alive");
